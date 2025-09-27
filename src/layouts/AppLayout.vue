01e14bc80a92ba725b278f9cb92a5ad9f9b3fd64
comsicomsa <inzotov94@gmail.com>
2025-09-27 23:59:09 +0400

    init commit

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="app-layout">
+    <slot />
+  </main>
+</template>
+
+<style lang="scss" scoped>
+@use "@/styles/variables" as *;
+
+.app-layout {
+  height: 100%;
+
+  padding-top: $gutter-y;
+  padding-bottom: $gutter-y;
+}
+</style>
